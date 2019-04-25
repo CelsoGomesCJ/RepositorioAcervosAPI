527b6faf2c067eeaf26b8a5871107d2f00b3c4e4
CelsoGomes2222 <celsogomes22@gmail.com>
2019-04-25 04:58:13 -0300

    Publicacoes dos acervos estrutura

---
+++ RepositoriosAcervosAPI/Controllers/PublicacaoController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RepositorioAcervosAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublicacaoController : ControllerBase
+    {
+        [HttpGet]
+        [Route("obtenhapublicacoes")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Publicacao/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        //Registrar Publicacao
+        [HttpPost]
+        [Route("registrarpublicacao")]
+        public void Post([FromBody] string value)
+        {
+
+        }
+
+    }
+}
---
+++ RepositoriosAcervosAPI/Dominio/Publicacao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RepositorioAcervosAPI.Dominio
+{
+    public class Publicacao
+    {
+        public long Id { get; set; }
+        public string titulo { get; set; }
+        public string subtitulo { get; set; }
+        public string palavrachave { get; set; }
+        public string resumo { get; set; }
+        public string autores { get; set; }
+        public byte[] documento { get; set; }
+    }
+}
